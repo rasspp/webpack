3a382978639436124fb5b008c9722e8384cd906c
ridhoakbar8977 <ridho8977@gmail.com>
2023-04-19 07:41:01 +0700

    setting contain file dev and prod

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,45 +1,10 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
-   mode: "production",
-   entry: "./src/index.js",
+   mode: "development",
    output: {
       path: path.resolve(__dirname, "dist"),
-      filename: "main.[contenthash].js",
+      filename: "main.js",
       clean: true,
-   },
-   plugins: [
-      new HtmlWebpackPlugin({
-         template: "./src/template.html",
-      }),
-   ],
-   watch: true,
-   devtool: false,
-   module: {
-      rules: [
-         {
-            test: /\.(?:js|mjs|cjs)$/,
-            exclude: /node_modules/,
-            use: {
-               loader: "babel-loader",
-               options: {
-                  presets: [["@babel/preset-env", { targets: "defaults" }]],
-                  plugins: ["@babel/plugin-proposal-class-properties"],
-               },
-            },
-         },
-         {
-            test: /\.s[ac]ss$/i,
-            use: [
-               // Creates `style` nodes from JS strings
-               "style-loader",
-               // Translates CSS into CommonJS
-               "css-loader",
-               // Compiles Sass to CSS
-               "sass-loader",
-            ],
-         },
-      ],
    },
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -3,43 +3,9 @@ const path = require("path");
 
 module.exports = {
    mode: "production",
-   entry: "./src/index.js",
    output: {
       path: path.resolve(__dirname, "dist"),
       filename: "main.[contenthash].js",
       clean: true,
-   },
-   plugins: [
-      new HtmlWebpackPlugin({
-         template: "./src/template.html",
-      }),
-   ],
-   watch: true,
-   devtool: false,
-   module: {
-      rules: [
-         {
-            test: /\.(?:js|mjs|cjs)$/,
-            exclude: /node_modules/,
-            use: {
-               loader: "babel-loader",
-               options: {
-                  presets: [["@babel/preset-env", { targets: "defaults" }]],
-                  plugins: ["@babel/plugin-proposal-class-properties"],
-               },
-            },
-         },
-         {
-            test: /\.s[ac]ss$/i,
-            use: [
-               // Creates `style` nodes from JS strings
-               "style-loader",
-               // Translates CSS into CommonJS
-               "css-loader",
-               // Compiles Sass to CSS
-               "sass-loader",
-            ],
-         },
-      ],
    },
 };
